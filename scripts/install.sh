ca8113d8be3843243196135aca8796cec4f10016
Timani Tunduwani <timani@users.noreply.github.com>
2014-09-02 17:15:39 -0700

    Moving the install tasks to the install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+## Clone terminus
+git clone https://github.com/pantheon-systems/terminus.git $HOME/.drush/terminus
+## Change to the terminus directory
+cd $HOME/.drush/terminus
+# Run composer in case there are any updates
+composer update --no-dev 

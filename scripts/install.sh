ae5853550448a5b59cf3cc570e41094f101d1c56
Timani Tunduwani <timani@users.noreply.github.com>
2014-09-02 21:35:37 -0700

    update install.sh to run setup tasks
    Parity for the install task https://gist.github.com/timani/3dc77c566b4ff8a75f12#file-travis-yml-L67

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-## Clone terminus
+# Dynamic hosts through Pantheon mean constantly checking interactively
+# that we mean to connect to an unknown host. We ignore those here.
+echo "StrictHostKeyChecking no" > ~/.ssh/config
+ 
+# Install Drush.
+composer global require drush/drush:6.2.0
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+ 
+# Install Terminus.
 git clone https://github.com/pantheon-systems/terminus.git $HOME/.drush/terminus
-## Change to the terminus directory
 cd $HOME/.drush/terminus
-# Run composer in case there are any updates
-composer update --no-dev 
+composer update --no-dev
+drush cc drush
+ 
+# Install additional test dependencies here (like Casper, Behat, etc).

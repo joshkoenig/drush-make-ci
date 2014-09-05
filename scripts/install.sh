db373f791d7d9d7bbd885d25789003b35b0339aa
Josh Koenig <josh@getpantheon.com>
2014-09-04 17:06:30 -0700

    trying out some encrypted key action

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,17 @@
 # Dynamic hosts through Pantheon mean constantly checking interactively
 # that we mean to connect to an unknown host. We ignore those here.
 echo "StrictHostKeyChecking no" > ~/.ssh/config
- 
+
 # Install Drush.
 composer global require drush/drush:6.2.0 -v
 export PATH="$HOME/.composer/vendor/bin:$PATH"
- 
+
 # Install Terminus.
 git clone https://github.com/pantheon-systems/terminus.git $HOME/.drush/terminus
 cd $HOME/.drush/terminus
 composer update --no-dev -v
-drush cc drush -v 
- 
+drush cc drush -v
+
 # Install additional test dependencies here (like Casper, Behat, etc).
+
+git clone ssh://codeserver.dev.$PUUID@codeserver.dev.$PUUID.drush.in:2222/~/repository.git $PNAME

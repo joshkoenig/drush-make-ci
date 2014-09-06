fa055f29ebda3842b8898cf398586f850a1bc85a
Josh Koenig <josh@getpantheon.com>
2014-09-05 19:24:56 -0700

    sftp mode is a must

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,7 +5,10 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Wipe out the DB
 drush psite-ewipe $PUUID $PENV -y
 
+# Insure we are in SFTP mode for a clean install
+drush psite-cmode $PUUID $PENV sftp
+
 # Install
-drush @pantheon.$PNAME.$PENV si si --account-pass=$RANDPASS --site-name="Travis Did It" -y
+drush @pantheon.$PNAME.$PENV si --account-pass=$RANDPASS --site-name="Travis Did It" -y
 
 # TODO: bhat test
